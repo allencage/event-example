427849786eeb54c08fa115d303cea607c7581c66
allencage <alingabriel.ciuca@gmail.com>
2017-04-24 16:04:33 +0200

    initial commit

---
+++ EventsExample/Pacient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EventsExample
+{
+	public class Pacient
+	{
+		private string _name;
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				if(_name != value)
+				{
+					OnNameChanged(this, new NameChangedEventArgs(value));
+					_name = value;
+				}
+			}
+		}
+
+		public event NameChangedDelegate OnNameChanged;
+	}
+
+	public delegate void NameChangedDelegate(object sender, NameChangedEventArgs args);
+
+	public class NameChangedEventArgs : EventArgs
+	{
+		private string _name;
+		public string Name { get { return _name; } }
+		public NameChangedEventArgs(string name)
+		{
+			_name = name;
+		}
+	}
+}
--- EventsExample/Program.cs
+++ EventsExample/Program.cs
@@ -10,6 +10,17 @@ namespace EventsExample
 	{
 		static void Main(string[] args)
 		{
+			var pacient = new Pacient();
+			pacient.OnNameChanged += Write;
+			pacient.Name = "Nea Ion";
+
+			Console.Read();
+		}
+
+
+		static void Write(object sender, NameChangedEventArgs args)
+		{
+			Console.WriteLine($"The name was changed to {args.Name}");
 		}
 	}
 }
